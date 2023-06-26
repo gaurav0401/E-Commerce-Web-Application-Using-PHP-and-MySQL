3244677deb644577cf704bbb2a0ee3c2475c2b7d
gaurav_04_01 <gaurav.tnv6970@gmail.com>
2023-06-27 00:07:00 +0530

    first commit

---
+++ files/connect.php
@@ -0,0 +1,16 @@
+<?php
+$servername = "localhost"; 
+$username = 'root'; 
+$password = '';  
+$myDB = 'oneshopy_project';
+
+try{
+	 $conn = new PDO("mysql:host=$servername;dbname=$myDB", $username, $password);
+	
+	
+	$conn->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+	//echo "Connection Successfully";
+} catch(PDOException $e){
+	echo "Connection failed : ".$e->getMessage();
+}
+?>
